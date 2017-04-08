8a0aa89a410821b2f87476b786c0824edfa0c42f
Tunmise Fatunmbi <danolez1fatuns@gmail.com>
2017-04-08 10:20:14 +0100

    Create Life-the-Universe-and-Everything.java

---
+++ Life-the-Universe-and-Everything.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Solution{
+static void lifeUniverseEverything(){
+        Scanner scanner = new Scanner(System.in);
+       int input = scanner.nextInt();
+       
+       if(input != 42){
+            System.out.println(input);            
+        while(0/input == 0){
+           int cont = scanner.nextInt();
+           if(cont!=42)
+                System.out.println(cont);
+           else
+               break;
+        }
+     }
+     }
+     public static void main(String[] args){
+     lifeUniverseEverything();
+     }
